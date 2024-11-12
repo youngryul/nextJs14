7bafe1228b867f25535e9209ae9073c7d7d674d5
PNS212624\Hansol <yrlee00@hansol.com>
2024-11-12 17:04:36 +0900

    11/12 layout, metadata 기능 추가

---
+++ app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import {Metadata} from "next";
+
+export const metadata :Metadata= {
+    title: "Home"
+
+}
+
+export default function Tomato() {
+    return (
+        <div>
+            <h1>Hello!</h1>
+        </div>
+    )
+}
